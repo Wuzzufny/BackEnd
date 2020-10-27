566f8f6d1d70336eda2fc589e65c548fa2ac442c
eng.sara.sultan@outlook.com <eng.sara.sultan@outlook.com>
2020-10-27 09:06:01 +0200

    new structure with identity

---
+++ BackEnd.Service/IService/IidentityServices.cs
@@ -0,0 +1,14 @@
+using BackEnd.BAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Service.ISercice
+{
+  public interface IidentityServices
+  {
+    Task<AuthenticationResult> RegisterAsync(string UserName,string Email,string Password, string Roles);
+    Task<AuthenticationResult> LoginAsync(string Email, string Password);
+  }
+}
